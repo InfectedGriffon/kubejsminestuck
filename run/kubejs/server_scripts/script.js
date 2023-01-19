32813fd21dbbffe7e2faeedbc1ea7669d79215ef
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-01-18 19:15:56 -0500

    more readable + stuff

--- run/kubejs/server_scripts/script.js
+++ run/kubejs/server_scripts/script.js
@@ -8,16 +8,20 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
+	
 	//combination
 	event.recipes.minestuck.combination("minecraft:coarse_dirt", "minecraft:dirt", "minecraft:gravel", "and")
-	event.remove({type:"minestuck:combination",output:"minecraft:diamond_ore"})
+	event.remove({type:"minestuck:combination", output:"minecraft:diamond_ore"})
 
-	//grist cost
-	event.recipes.minestuck.grist_cost("minecraft:magenta_stained_glass", {"minestuck:build":4,"minestuck:tar":1})
-	event.remove({type:"minestuck:grist_cost",input:"minecraft:dirt"})
+	//adding and removing grist costs
+	event.recipes.minestuck.grist_cost("minecraft:magenta_stained_glass", {"minestuck:build":4, "minestuck:tar":1})
+	event.remove({type:"minestuck:grist_cost", input:"minecraft:dirt"})
+	
+	//using grist.of and gristset.from
 	event.recipes.minestuckGristCost("minecraft:dirt", Grist.of("build", 3))
-	event.recipes.minestuck.grist_cost("minecraft:coarse_dirt", GristSet.from(Grist.of("build",2),Grist.of("amber",4)))
+	event.recipes.minestuck.grist_cost("minecraft:coarse_dirt", 
+		GristSet.from(  Grist.of("build",2),  Grist.of("amber",4)  ))
 
 	//irradiating
-	event.recipes.minestuck.irradiating("minecraft:emerald", "minecraft:diamond").xp(0.1) //recipe type not in jei
+	event.recipes.minestuck.irradiating("minecraft:emerald", "minecraft:diamond").xp(0.1)
 })
