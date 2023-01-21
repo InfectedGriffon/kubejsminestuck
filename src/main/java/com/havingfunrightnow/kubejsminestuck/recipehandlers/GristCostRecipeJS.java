23223357d5fff17eacef4a91d8e7a385e883a991
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-01-21 18:40:19 -0500

    better jsonification

--- src/main/java/com/havingfunrightnow/kubejsminestuck/recipehandlers/GristCostRecipeJS.java
+++ src/main/java/com/havingfunrightnow/kubejsminestuck/recipehandlers/GristCostRecipeJS.java
@@ -2,34 +2,53 @@ package com.havingfunrightnow.kubejsminestuck.recipehandlers;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.mraof.minestuck.alchemy.GristAmount;
 import com.mraof.minestuck.alchemy.GristSet;
 import com.mraof.minestuck.alchemy.GristTypes;
+
 import dev.latvian.mods.kubejs.recipe.RecipeJS;
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import dev.latvian.mods.kubejs.util.ListJS;
 
 public class GristCostRecipeJS extends RecipeJS {
 
-    // stores grist costs
     public JsonElement grist_cost;
 
     @Override
     public void create(ListJS args) {
         inputItems.add(parseIngredientItem(args.get(0)));
-        if (args.get(1) instanceof GristAmount grist) {
-            grist_cost = new Gson().fromJson(getGristString(grist), JsonElement.class); //i love json ❤
-        } else if (args.get(1) instanceof GristSet gristSet) {
-            grist_cost = gristSet.serialize();
+
+        if (args.size() == 3) {
+            grist_cost = jsonify(args.get(1), args.get(2));
+        } else if (args.size() == 2) {
+            grist_cost = jsonify(args.get(1));
         } else {
-            grist_cost = new Gson().toJsonTree(args.get(1)); // mapjs -> jsonelement
+            ConsoleJS.SERVER.error("Too many arguments! Try using Grist.set()!");
         }
     }
 
-    public String getGristString(GristAmount grist) {
-        if (grist.getType()==GristTypes.ZILLIUM.get() && grist.getAmount()==Long.MIN_VALUE) {
-            return "{}";
+    /*
+     * take some object and turns it into a json element
+     */
+    public JsonElement jsonify(Object thisThing) {
+        if (thisThing instanceof GristAmount grist) {
+            if (grist.getType() == GristTypes.ZILLIUM.get() && grist.getAmount() == Long.MIN_VALUE) {
+                return JsonParser.parseString("{}");
+            }
+            return jsonify(grist.getType(), grist.getAmount());
+        } else if (thisThing instanceof GristSet gristSet) { //gristset has built-in serializer
+            return gristSet.serialize();
+        } else {
+            return new Gson().toJsonTree(thisThing);
         }
-        return "{\""+grist.getType().toString()+"\":"+String.valueOf(grist.getAmount())+"}";
+    }
+
+    /*
+     * takes two objects and turns them into a json element
+     */
+    public JsonElement jsonify(Object key, Object value) {
+        return JsonParser.parseString("{\""+key.toString()+"\":"+value.toString()+"}");
     }
 
     @Override
