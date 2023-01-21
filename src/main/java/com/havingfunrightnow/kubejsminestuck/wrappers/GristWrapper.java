84c2aa7d2ac4adfbdf3d984df0bdf9a3a8fd5ada
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-01-21 18:39:16 -0500

    remove gristsetwrapper

--- src/main/java/com/havingfunrightnow/kubejsminestuck/wrappers/GristWrapper.java
+++ src/main/java/com/havingfunrightnow/kubejsminestuck/wrappers/GristWrapper.java
@@ -1,6 +1,7 @@
 package com.havingfunrightnow.kubejsminestuck.wrappers;
 
 import com.mraof.minestuck.alchemy.GristAmount;
+import com.mraof.minestuck.alchemy.GristSet;
 import com.mraof.minestuck.alchemy.GristType;
 import com.mraof.minestuck.alchemy.GristTypes;
 
@@ -8,5 +9,8 @@ public class GristWrapper {
     public final static GristAmount free = new GristAmount(GristTypes.ZILLIUM, Long.MIN_VALUE);
     public static GristAmount of(GristType type, int amount) {
         return new GristAmount(type, amount);
+    }
+    public static GristSet set(GristAmount... grist) {
+        return new GristSet(grist);
     }
 }
