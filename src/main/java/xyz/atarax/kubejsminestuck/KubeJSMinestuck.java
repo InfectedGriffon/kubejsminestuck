74dac138a48026967b0530a5ab9710781f0cbdd5
InfectedGriffon <roofusjoofusjambeardiv@gmail.com>
2026-06-04 01:05:07 -0400

    Initial Commmmit (neoforge mdk)

---
+++ src/main/java/xyz/atarax/kubejsminestuck/KubeJSMinestuck.java
@@ -0,0 +1,8 @@
+package xyz.atarax.kubejsminestuck;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(KubeJSMinestuck.MOD_ID)
+public class KubeJSMinestuck {
+    public static final String MOD_ID = "kubejsminestuck";
+}
---
+++ src/main/java/xyz/atarax/kubejsminestuck/KubeJSMinestuckPlugin.java
@@ -0,0 +1,7 @@
+package xyz.atarax.kubejsminestuck;
+
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+
+public class KubeJSMinestuckPlugin implements KubeJSPlugin {
+    // TODO
+}
